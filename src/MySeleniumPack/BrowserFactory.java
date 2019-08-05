e7cfcac895c3387baf006b33ed3f7135f2cdfdef
13Suman <sksk6122@gmail.com>
2019-08-05 15:23:44 +0530

    mouse operation

---
+++ src/MySeleniumPack/BrowserFactory.java
@@ -0,0 +1,30 @@
+package MySeleniumPack;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserFactory {
+
+
+	public static WebDriver launch (String browserName){
+		
+	WebDriver driver = null;
+	
+		if(browserName.equalsIgnoreCase("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", "./exe files/chromedriver.exe");
+			driver = new ChromeDriver();
+			
+	}
+		else  if (browserName.equalsIgnoreCase("firefox")){
+		
+			System.setProperty("webdriver.firefox.driver", "./exe files/geckodriver.exe");
+			driver = new FirefoxDriver();
+			
+			
+		}
+		
+		return driver;
+
+}
+}
